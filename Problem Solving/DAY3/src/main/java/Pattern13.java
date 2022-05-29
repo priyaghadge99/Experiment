80473b12937724260766e046e74a1e9b325ecdc2
priyaghadge99 <priyaghadge59@gmail.com>
2022-05-29 18:12:08 +0530

    Pattern logic added

--- Problem Solving/DAY3/src/main/java/Pattern13.java
+++ Problem Solving/DAY3/src/main/java/Pattern13.java
@@ -8,11 +8,12 @@ public class Pattern13 {
     public static void main(String[] args) {
         int row=4;
         for (int i=1;i<=row;i++){
-
-            for (int j=row-i;j>=1;j--){      // 4-1 = 3   3  2 1
+            //to print spaces
+            for (int j=row-i;j>=1;j--){      // 4-1 = 3   3  2 1   3space
                 System.out.print(" ");
 
             }
+            //to print star
             for (int j=1;j<=i;j++){
                 System.out.print("* ");
             }
