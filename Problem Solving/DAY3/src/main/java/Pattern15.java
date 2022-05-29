566d444ce613f6ed2cbc8c77099286b89c025f0c
priyaghadge99 <priyaghadge59@gmail.com>
2022-05-29 18:48:27 +0530

    Pattern15 logic added

--- Problem Solving/DAY3/src/main/java/Pattern15.java
+++ Problem Solving/DAY3/src/main/java/Pattern15.java
@@ -6,14 +6,21 @@ public class Pattern15 {
     public static void main(String[] args) {
 
         for (int i=1;i<=3;i++) {
-            if (i % 2 == 0)//space{
+            if (i== 2 )//space{
             {
                 System.out.print(" ");
             }
 
                 for (int j = 1; j <= i; j++) {
 
-                    System.out.print(i  +" " +i);
+                    for (int k=1;k==j;k++) {
+                        if (i%2==0){
+                            System.out.print(i);
+                        }
+                        else {
+                            System.out.print(i + " " + i);
+                        }
+                    }
                 }
 //           }
                 System.out.println();
@@ -22,3 +29,9 @@ public class Pattern15 {
 
     }
 }
+
+
+//1 1
+// 2 22 2
+//3 33 33 3
+//solved
