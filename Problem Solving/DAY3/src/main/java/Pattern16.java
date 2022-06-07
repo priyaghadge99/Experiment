c86b3a6a309588e032fdd4f6ae7260c6d385027f
= <priyaghadge59@gmail.com>
2022-06-07 16:03:58 +0530

    Pattern16 logic added

--- Problem Solving/DAY3/src/main/java/Pattern16.java
+++ Problem Solving/DAY3/src/main/java/Pattern16.java
@@ -4,17 +4,21 @@
 // *----*
 public class Pattern16 {
     public static void main(String[] args) {
-        for (int i =1;i<=3; i++) {
-            for (int j = 3 - i; j >= 1; j--) {    //3-1 =2  2 ,1
-                System.out.print("-");
+        for (int i =0;i < 3; i++) {
+            for (int j = 2 - i; j >= 1; j--) {    //2-i =2-0  =2 ,1
+                System.out.print("-");             //--
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+            for (int m = 1; m <= 2; m++) {
+                System.out.print("*");             //*
+                if (m==1){
+                    for (int k=0;k<2*i;k++){         // 2*i =2*0 = 0 false condition :not print for first one
+                        System.out.print("-");
+                    }
+                }
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            for (int j = 3 - i; j >= 1; j--) {    //3-1 =2  2 ,1
+ //-----------------------------------------------------------
+
+            for (int j = 0; j < 3-i-1; j++) {   //2-i =2-0  =2 ,1
                 System.out.print("-");
             }
             System.out.println();
