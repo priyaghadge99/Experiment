4dc24335f9caff737cc90777c3ee8017973d2a42
priyaghadge99 <priyaghadge59@gmail.com>
2022-06-18 17:00:47 +0530

    _*_

--- DS&Algo/Sorting/src/QuickSort.java
+++ DS&Algo/Sorting/src/QuickSort.java
@@ -1,6 +1,7 @@
 //consider last element : pivot
 //put smaller element to left of pivot
 public class QuickSort {
+    /* author :priya */
     public static void main(String[] args) {
 
         int arr[] = {24,9,29,14,19,27};
