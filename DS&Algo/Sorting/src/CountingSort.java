746c271cbfac303c4edaa98fb5c2822b8045f5c9
= <priyaghadge59@gmail.com>
2022-06-23 22:49:20 +0530

    Counting Sort Logic added

--- DS&Algo/Sorting/src/CountingSort.java
+++ DS&Algo/Sorting/src/CountingSort.java
@@ -1,62 +1,53 @@
+import java.util.Arrays;
+
 public class CountingSort {
 
 
     private static void countingSort(int[] arr) {   // different main
-      int outputArr[]= new int[arr.length];
+        int outputArr[] = new int[arr.length];
 
-      int n= arr.length;
-        int[] output = new int[0];
-        int[] countArr = new int[arr.length];
-      for (int i=0;i< arr.length;i++){
-         
-          countArr[i] = 0;
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] countArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            countArr[i] = 0;     //First store zero
 
-      }
-      //First store zero
-        for (int j:countArr){
-//            System.out.print(j +" ");
         }
         //count each element in array
-      for (int i=0;i<arr.length;i++){
-          ++countArr[arr[i]];
+        for (int i = 0; i < n; i++) {
+            ++countArr[arr[i]];
 
-      }
-
-        for (int j:countArr){
-            System.out.print(j +" ");
         }
-
+            System.out.println("CountArray " +Arrays.toString(countArr));
 
         //change position of count[i]
-        for(int i=1;i<=arr.length-1;++i){
-            countArr[i] += countArr[i-1];
+
+        for (int i = 1; i <= arr.length - 1; ++i) {
+            countArr[i] += countArr[i - 1];
         }
         // Build the output character array
         // To make it stable we are operating in reverse order.
 
         for (int i = n - 1; i >= 0; i--) {
-          
+
             output[countArr[arr[i]] - 1] = arr[i];
             --countArr[arr[i]];
         }
 
 
-        for (int i = 0; i < n; ++i){
+        for (int i = 0; i < n; ++i) {
             arr[i] = output[i];
+        }
+
+
     }
-
-
-  }
 
     public static void main(String[] args) {
-        int arr[] ={ 1,0,3,1,3,1};
-
+        int arr[] = {1, 0, 3, 1, 3, 1};
         countingSort(arr);
-        System.out.print("Sorted character array is ");
-        for (int i = 0; i < arr.length; ++i)
-            System.out.print(arr[i]);
+        System.out.print("Sorted character array is " + Arrays.toString(arr));
+
+
     }
-
-
 }
 
