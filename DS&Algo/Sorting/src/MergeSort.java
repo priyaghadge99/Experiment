9f209f43d4cdceddd7e8242bec7b247934126cba
= <priyaghadge59@gmail.com>
2022-06-27 19:56:05 +0530

    MergeSort Added

--- DS&Algo/Sorting/src/MergeSort.java
+++ DS&Algo/Sorting/src/MergeSort.java
@@ -1,46 +1,70 @@
+import java.util.Arrays;
+
 public class MergeSort {
 
-    void sort(int[] arr,int l , int r){
-       if (l<r){
-           int m= l+(r-l)/2
-           sort(arr,l,m);
-           sort(arr,m+1,r);
-           //then call merge function to merge sort
-           merge(arr,l,m,r);
-       }
+    void sort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            //then call merge function to merge sort
+            merge(arr, l, m, r);
+        }
 
     }
+
     //merge logic
-    void merge(int arr[] ,int l ,int m ,int r){
+    void merge(int arr[], int l, int m, int r) {
         //create a left and right array
-        int n1= m-1;
-        int n2 = r+1;
+        int n1 = m-l+1;
+        int n2 = r-m;
 
         int L[] = new int[n1];
         int R[] = new int[n2];  //created temp array
-        int i =0 ,j=0;
-        int k = l;
-        while (i<n1 && j<n2){
-            if (L[i] <=R[i]) {
-                arr[k] = L[i];
-                i++;
-            }
-            else{
-                arr[k] = R[j]
-                j++;
-                 }
+        //add value in this array
 
-        k++;
+        for (int i = 0; i < n1;i++) {
+            L[i] = arr[l+ i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[m +1+ j];
         }
 
 
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[i]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        //remaining in L
+        while(i<n1){
+            arr[k] = L[i];
+                    i++;
+            k++;
+        }
+        //remaining in R which is greater
+        while(j<n2){
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
 
     }
     public static void main(String[] args) {
-        int[] arr = { 12, 11, 13, 5, 6, 7}
+        int[] arr = {12, 11, 13, 5, 6, 7};
         MergeSort ob = new MergeSort();
 
-        ob.sort(arr,0,arr.length-1);
+        ob.sort(arr, 0, arr.length - 1);
+        System.out.println(" After MergeSort"+ Arrays.toString(arr));
     }
 }
 
