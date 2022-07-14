5f84fcce7bf6b24132a76baadda58d55543cf962
priyaghadge99 <priyaghadge59@gmail.com>
2022-07-15 01:19:30 +0530

    Kruskal Algo added

--- DS&Algo/Graphs/src/Kgraph.java
+++ DS&Algo/Graphs/src/Kgraph.java
@@ -15,10 +15,10 @@ public class Kgraph{
     }
 
      //constructor
-     Kgraph(int v ,int e) {
+    public Kgraph(int v ,int e) {
          this.vertices= v;
          this.edges= e;
-         edge = new Edge[this.edge.length];
+         edge = new Edge[this.vertices];
          for (int i=0;i<v;i++){
             edge[i] = new Edge();
          }
@@ -51,55 +51,76 @@ public class Kgraph{
     }
 
     //detect cycle is present or not
-    public  int kruskalMst(Kgraph kgraph) {
+    public void kruskalMst(Kgraph kgraph) {
         //
-        int vertice = kgraph.vertices;
-        int edge = kgraph.edges;
+        int vertices1 = kgraph.vertices;
+        int edge1 = kgraph.edges;
 
         // This will store the resultant MST
-        Edge result[] = new Edge[edge];
+        Edge result[] = new Edge[edge1];
 
         for (int j = 0 ;j<edges;j++){
             result[j] = new Edge(); 
         }
 
      //sort the edges
-          Arrays.sort(kgraph.edge);
+          Arrays.sort(edge);
 
          int e=0 ; //index for
-        Subset[] subset = new Subset[vertice];
+        Subset[] subset = new Subset[vertices1];
 
         // Allocate memory for creating V subsets
         for (int i = 0; i < vertices; ++i)
             subset[i] = new Subset();
        //make all rank as 0 and parent as i
-        for (int i=0;i<vertice;i++){
+        for (int i=0;i<vertices1;i++){
             subset[i].parent = i;
             subset[i].rank =0;
         }
         int i=0; //for increamenting edge
-        while (e)
+        while (e<vertices1-1){
+            //Pick the smallest edge. And increment
+            // the index for next iteration
+            Edge nextEdge = edge[i++];  //taking smallest edge
 
+            int x = find(subset, nextEdge.src);
+            int y = find(subset,nextEdge.des);
 
-
-
+            if (x!=y){
+                result[e++] = nextEdge;  //need to add edge in result array
+                union(subset,x,y);
+            }
+        }
+        System.out.println("Following MSt Edges");
+        int mincost=0;
+       for ( i=0;i< result.length;i++){
+           System.out.println(result[i].src +" " + result[i].des +" " + " ==  "+result[i].wt);
+           mincost += result[i].wt;
+       }
+        System.out.println("mininum cost :" +mincost);
     }
-
-
-
     public static void main(String[] arg){
 
-         Kgraph kgraph = new Kgraph(3,3);
+         Kgraph kgraph = new Kgraph(4,4);
 
            kgraph.edge[0].src = 0;
            kgraph.edge[0].des = 1;
+           kgraph.edge[0].wt = 23;
 
            kgraph.edge[1].src = 1;
            kgraph.edge[1].des= 2;
+           kgraph.edge[1].wt= 2;
 
            kgraph.edge[2].src= 0;
            kgraph.edge[2].des =2;
+           kgraph.edge[2].wt=21;
 
+           kgraph.edge[3].src= 1;
+           kgraph.edge[3].des = 3;
+           kgraph.edge[3].wt=21;
+
+
+           kgraph.kruskalMst(kgraph);
 
     }
 
